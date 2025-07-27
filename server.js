0aad053250f09d164a29ad2c4ec5f166e0f9c050
RickDeb2004 <debanjanrick04@gmail.com>
2025-07-27 10:27:49 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-const express = require("express");
-require("dotenv").config();
 
-const app = express();
-app.use(express.json());
-
-const adminRoutes = require("./routes/admin");
-const userRoutes = require("./routes/users");
-const vendorRoutes = require("./routes/vendors");
-app.use("/api", adminRoutes);
-app.use("/api", userRoutes);
-app.use("/api", vendorRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// server.js
+// For local development, use the following code snippet
 // const express = require("express");
-// const cors = require("cors");
 // require("dotenv").config();
 
-// const adminRoutes = require("./routes/admin");
-// const vendorRoutes = require("./routes/vendors");
-// const userRoutes = require("./routes/users");
-
 // const app = express();
-
-// app.use(cors());
 // app.use(express.json());
 
+// const adminRoutes = require("./routes/admin");
+// const userRoutes = require("./routes/users");
+// const vendorRoutes = require("./routes/vendors");
 // app.use("/api", adminRoutes);
-// app.use("/api", vendorRoutes);
 // app.use("/api", userRoutes);
+// app.use("/api", vendorRoutes);
 
-// module.exports = app; // export instead of listen
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// When you will deploy use the below one code snippet
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const adminRoutes = require("./routes/admin");
+const vendorRoutes = require("./routes/vendors");
+const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", adminRoutes);
+app.use("/api", vendorRoutes);
+app.use("/api", userRoutes);
+
+module.exports = app; // export instead of listen
