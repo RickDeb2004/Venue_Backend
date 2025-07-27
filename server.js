85f7d6cafb635723ce96d3fdc5338d8519ec976e
RickDeb2004 <debanjanrick04@gmail.com>
2025-07-27 13:43:19 +0530

    prod code to render

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const { app } = require("./api/[[...slug]]");
+// server.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
+const adminRoutes = require("./routes/admin");
+const vendorRoutes = require("./routes/vendors");
+const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", adminRoutes);
+app.use("/api", vendorRoutes);
+app.use("/api", userRoutes);
+
+// Start the server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
